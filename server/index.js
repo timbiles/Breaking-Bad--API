385d597764d525aaf2baf1db2ada50db69a72b54
Tim Biles <timbilestimbiles@gmail.com>
2018-10-18 11:43:52 -0500

    run build

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
+const path = require('path');
 const rateLimit = require('express-rate-limit');
 
 const port = process.env.SERVER_PORT || 3001;
-
 
 const {
   getPeople,
@@ -13,13 +13,23 @@ const {
   getRandomChar,
   getAll
 } = require('./Ctrl/characterCtrl');
-const { getQuotes, getQuoteById, getRandomQuote, getQuoteByAuthor } = require('./Ctrl/quoteCtrl');
+const {
+  getQuotes,
+  getQuoteById,
+  getRandomQuote,
+  getQuoteByAuthor
+} = require('./Ctrl/quoteCtrl');
 const { getEpisodes, getEpisodesById } = require('./Ctrl/episodeCtrl');
-const { getDeaths, getDeathTotal, getRandomDeath } = require('./Ctrl/deathCtrl')
-
+const {
+  getDeaths,
+  getDeathTotal,
+  getRandomDeath
+} = require('./Ctrl/deathCtrl');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(express.static(`${__dirname}/../build`));
 
 massive(process.env.CONNECTION_STRING)
   .then(db => {
@@ -29,13 +39,13 @@ massive(process.env.CONNECTION_STRING)
     console.log('Database connection error', err);
   });
 
-  const limiter = rateLimit({
-    windowMs: 1000 * 60 * 60 * 24,
-    max: 10000,
-    message: "Too many hits. Plz try again l8tr.."
-  })
-  
-  app.use(limiter)
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 60 * 24,
+  max: 1000,
+  message: 'Too many hits. Plz try again l8tr..'
+});
+
+app.use(limiter);
 
 // Character endpoints
 app.get('/api/characters', getPeople);
@@ -44,20 +54,25 @@ app.get('/api/character/random', getRandomChar);
 
 // Quote endpoints
 app.get('/api/quotes', getQuotes);
-app.get('/api/quotes/:id', getQuoteById)
+app.get('/api/quotes/:id', getQuoteById);
 app.get('/api/quote/random', getRandomQuote);
 app.get('/api/quote', getQuoteByAuthor);
 
 // Episode endpoints
 app.get('/api/episodes', getEpisodes);
-app.get('/api/episodes/:id', getEpisodesById)
+app.get('/api/episodes/:id', getEpisodesById);
 
 // Death count
-app.get('/api/deaths', getDeaths)
-app.get('/api/death-count', getDeathTotal)
-app.get('/api/random-death', getRandomDeath)
+app.get('/api/deaths', getDeaths);
+app.get('/api/death-count', getDeathTotal);
+app.get('/api/random-death', getRandomDeath);
 
-app.get('/api', getAll)
+app.get('/api', getAll);
+
+//run build
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
