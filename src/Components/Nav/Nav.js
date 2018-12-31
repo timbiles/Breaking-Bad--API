896d2b12bcd3df71a6492cacf9671e1ea07a5cb6
Tim Biles <timbilestimbiles@gmail.com>
2018-12-31 14:04:04 -0600

    testing nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {StyledLink, MainWrapper, Wrapper, Input, Btn} from '../../styles/nav';
+import { StyledLink, MainWrapper, Wrapper, Input, Btn } from '../../styles/nav';
 
 export default class Nav extends Component {
   state = {
     nav: ['Home', 'About', 'Documentation'],
     data: [],
-    search: ''
-  }
+    search: 'ice trays'
+  };
 
   componentDidMount() {
-    axios('/api/complete').then((res) => {
-      this.setState({data: res.data})
-    })
+    axios('/api/complete').then(res => {
+      this.setState({ data: res.data });
+    });
   }
-  
+
   render() {
-    const {data, search} = this.state
-    console.log('data', this.state.data)
+    const { data, search } = this.state;
+    console.log('data', this.state.data);
 
-    let results = []
+    let results = [];
 
-      for(var i=0; i<data.length; i++) {
-        for(let key in data[i]) {
-          if (typeof data[i][key] === 'string'){
-            if(data[i][key].indexOf(search)!=-1) {
-              results.push(data[i]);
-            }
+    for (var i = 0; i < data.length; i++) {
+      for (let key in data[i]) {
+        if (typeof data[i][key] === 'string' && search !== '') {
+          if (data[i][key].indexOf(search) != -1) {
+            results.push(data[i]);
+          }
         }
       }
     }
-      console.log(results)
+    console.log(results);
 
     const map = this.state.nav.map((e, i) => {
       return (
@@ -41,13 +41,20 @@ export default class Nav extends Component {
     });
 
     return (
-      <MainWrapper className='header'>
+      <MainWrapper className="header">
         <Wrapper main>{map}</Wrapper>
         <Wrapper secondary>
-          <Input type="text" placeholder='Search Breaking Bad API' name="" id="" width='300px'/>
+          <Input
+            type="text"
+            placeholder="Search Breaking Bad API"
+            onChange={e => this.setState({ search: e.target.value })}
+            name=""
+            id=""
+            width="300px"
+          />
           <Btn>Press</Btn>
-          </Wrapper>
+        </Wrapper>
       </MainWrapper>
     );
-  };
+  }
 }
