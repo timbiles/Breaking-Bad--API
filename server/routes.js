97e536457d585d962b0dd3d6b16dc28310c4c667
Tim Biles <timbilestimbiles@gmail.com>
2019-03-06 14:17:26 -0600

    small edits

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,3 @@
-const express = require('express');
-
 const {
   getPeople,
   getPeopleById,
--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -12,8 +12,6 @@ class Docs extends Component {
       .scrollIntoView({ block: 'start', behavior: 'smooth' });
   };
 
-  //document.getElementById('id').scrollIntoView({block: start, bahavior: 'smooth'})
-
   render() {
     const navMap = navigation.map((e, i) => {
       return React.createElement(
