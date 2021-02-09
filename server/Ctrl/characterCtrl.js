2c9f3a8f24896157c0ed3a72dfb1e375f1f101f0
Tim Biles <timbilestimbiles@gmail.com>
2021-02-08 18:42:14 -0600

    character appearance correction for better call saul

--- server/Ctrl/characterCtrl.js
+++ server/Ctrl/characterCtrl.js
@@ -5,23 +5,27 @@ const { capitalizeFirstLetter } = require('../utils/character');
 let occ = [],
   app = [],
   betterApp = [];
-const aMap = arr => {
-  return arr ? arr.map(el => +el) : [];
+const aMap = (arr) => {
+  return arr ? arr.map((el) => +el) : [];
 };
 
-const charactersFunc = func => {
+const charactersFunc = (func) => {
   func.map((e, i) => {
-    e.occupation && occ.push(e.occupation.split(',').map(el => el.trim()));
+    e.occupation && occ.push(e.occupation.split(',').map((el) => el.trim()));
     e.appearance && app.push(e.appearance.split(','));
-    betterApp.push(e.better_call_saul_appearance && e.better_call_saul_appearance.split(','));
+    betterApp.push(
+      e.better_call_saul_appearance && e.better_call_saul_appearance.split(',')
+    );
 
     if (!e.category) {
       e.category = 'Breaking Bad';
-    } 
+    }
 
     e.occupation = occ[i];
     if (e.appearance) {
       e.appearance = aMap(app[i]);
+    } else {
+      e.appearance = [];
     }
     e.better_call_saul_appearance = aMap(betterApp[i]);
     e.birthday = e.birthday
@@ -34,16 +38,16 @@ const getPeople = (req, res) => {
   const db = req.app.get('db');
   const { limit, name, offset, category } = req.query;
 
-  if(category) {
-    db.characters.get_char_by_category([`%${category}%`]).then(response => {
+  if (category) {
+    db.characters.get_char_by_category([`%${category}%`]).then((response) => {
       charactersFunc(response);
-      res.status(200)
-      .send(
-        limit || offset ? response.splice(offset || 0, limit) : response
-      )
-    })
+      res
+        .status(200)
+        .send(limit || offset ? response.splice(offset || 0, limit) : response);
+    });
     occ = [];
     app = [];
+    betterApp = [];
     return;
   }
 
@@ -51,13 +55,13 @@ const getPeople = (req, res) => {
     name &&
     name
       .split(' ')
-      .map(e => {
+      .map((e) => {
         return e.charAt(0).toUpperCase() + e.slice(1);
       })
       .join(' ');
 
   !name
-    ? db.characters.get_characters().then(response => {
+    ? db.characters.get_characters().then((response) => {
         charactersFunc(response);
         res
           .status(200)
@@ -65,17 +69,20 @@ const getPeople = (req, res) => {
             limit || offset ? response.splice(offset || 0, limit) : response
           );
       })
-    : db.characters.get_char_by_name([newName]).then(response => {
+    : db.characters.get_char_by_name([newName]).then((response) => {
         if (!response.length) {
-          const percentName = newName ? `%${capitalizeFirstLetter(newName)}%` : '%%';
+          const percentName = newName
+            ? `%${capitalizeFirstLetter(newName)}%`
+            : '%%';
           db.characters.get_char_closest(percentName).then((secondResponse) => {
             charactersFunc(secondResponse);
             res.status(200).send(
               // secondResponse
-            limit || offset ? secondResponse.splice(offset || 0, limit) : secondResponse
-
-              );
-          })
+              limit || offset
+                ? secondResponse.splice(offset || 0, limit)
+                : secondResponse
+            );
+          });
         } else {
           charactersFunc(response);
           res.status(200).send(response);
@@ -83,6 +90,7 @@ const getPeople = (req, res) => {
       });
   occ = [];
   app = [];
+  betterApp = [];
 };
 
 const getPeopleFooter = (req, res) => {
@@ -93,13 +101,13 @@ const getPeopleFooter = (req, res) => {
     name &&
     name
       .split(' ')
-      .map(e => {
+      .map((e) => {
         return e.charAt(0).toUpperCase() + e.slice(1);
       })
       .join(' ');
 
   !name
-    ? db.characters.get_characters().then(response => {
+    ? db.characters.get_characters().then((response) => {
         charactersFunc(response);
         res
           .status(200)
@@ -107,12 +115,13 @@ const getPeopleFooter = (req, res) => {
             limit || offset ? response.splice(offset || 0, limit) : response
           );
       })
-    : db.characters.get_char_by_name([newName]).then(response => {
+    : db.characters.get_char_by_name([newName]).then((response) => {
         charactersFunc(response);
         res.status(200).send(response);
       });
   occ = [];
   app = [];
+  betterApp = [];
 };
 
 const getPeopleById = (req, res) => {
@@ -121,16 +130,17 @@ const getPeopleById = (req, res) => {
 
   db.characters
     .get_char_by_id([id])
-    .then(resp => {
-      charactersFunc(resp)
+    .then((resp) => {
+      charactersFunc(resp);
       res.status(200).send(resp);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 
-    occ = [];
-    app = [];
+  occ = [];
+  app = [];
+  betterApp = [];
 };
 
 const getRandomChar = (req, res) => {
@@ -141,7 +151,7 @@ const getRandomChar = (req, res) => {
 
   db.characters
     .get_random_char([limit || 1])
-    .then(resp => {
+    .then((resp) => {
       resp.map((e, i) => {
         e.occupation && o.push(e.occupation.split(','));
         e.appearance && a.push(e.appearance.split(','));
@@ -153,7 +163,7 @@ const getRandomChar = (req, res) => {
       });
       res.status(200).send(resp);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 };
@@ -165,27 +175,30 @@ const getHomePage = (req, res) => {
   const a = [];
   const betterCallA = [];
 
-  if(category) {
-    db.characters.get_char_by_category_homepage([`%${category}%`, limit || 1])
-    .then(resp => {
-      resp.map((e, i) => {
-        if (!e.category) {
-          e.category = 'Breaking Bad';
-        } 
-        e.occupation && o.push(e.occupation.split(','));
-        e.appearance && a.push(e.appearance.split(','));
-        betterCallA.push(e.better_call_saul_appearance && e.better_call_saul_appearance.split(','));
+  if (category) {
+    db.characters
+      .get_char_by_category_homepage([`%${category}%`, limit || 1])
+      .then((resp) => {
+        resp.map((e, i) => {
+          if (!e.category) {
+            e.category = 'Breaking Bad';
+          }
+          e.occupation && o.push(e.occupation.split(','));
+          e.appearance && a.push(e.appearance.split(','));
+          betterCallA.push(
+            e.better_call_saul_appearance &&
+              e.better_call_saul_appearance.split(',')
+          );
 
-        e.occupation = o[i];
-        e.appearance = aMap(a[i]);
-        e.better_call_saul_appearance = aMap(betterCallA[i]);
+          e.occupation = o[i];
+          e.appearance = aMap(a[i]);
+          e.better_call_saul_appearance = aMap(betterCallA[i]);
+        });
+        // res.status(200).send(resp);
+        res
+          .status(200)
+          .send(limit || offset ? resp.splice(offset || 0, limit) : resp);
       });
-      // res.status(200).send(resp);
-      res.status(200)
-      .send(
-        limit || offset ? resp.splice(offset || 0, limit) : resp
-      )
-    })
     // .then(response => {
     //   charactersFunc(response);
     //   res.status(200)
@@ -198,11 +211,11 @@ const getHomePage = (req, res) => {
 
   db.characters
     .get_random_char([limit || 1])
-    .then(resp => {
+    .then((resp) => {
       resp.map((e, i) => {
         if (!e.category) {
           e.category = 'Breaking Bad';
-        } 
+        }
         e.occupation && o.push(e.occupation.split(','));
         a.push(e.appearance.split(','));
 
@@ -211,7 +224,7 @@ const getHomePage = (req, res) => {
       });
       res.status(200).send(resp);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 };
@@ -221,10 +234,10 @@ const getCharLimit = (req, res) => {
 
   db.characters
     .get_char_limit([limit, offset])
-    .then(resp => {
+    .then((resp) => {
       res.status(200).send(resp);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 };
@@ -237,16 +250,16 @@ const getEverything = async (req, res) => {
   const db = req.app.get('db');
   let everything = [];
 
-  await db.characters.get_characters().then(resp => {
+  await db.characters.get_characters().then((resp) => {
     everything.push(...resp);
   });
-  await db.episodes.get_episodes().then(resp => {
+  await db.episodes.get_episodes().then((resp) => {
     everything.push(...resp);
   });
-  await db.quotes.get_quotes().then(resp => {
+  await db.quotes.get_quotes().then((resp) => {
     everything.push(...resp);
   });
-  await db.death.get_deaths().then(resp => {
+  await db.death.get_deaths().then((resp) => {
     everything.push(...resp);
   });
 
@@ -260,5 +273,5 @@ module.exports = {
   getHomePage,
   getAll,
   getEverything,
-  getPeopleFooter
+  getPeopleFooter,
 };
